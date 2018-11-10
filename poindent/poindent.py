bb13eb04ccbd282065e8e54076e70a74f8045927
Julien Palard <julien@palard.fr>
2018-11-10 15:40:26 +0100

    Once you go black, you never go back.

--- poindent/poindent.py
+++ poindent/poindent.py
@@ -13,32 +13,42 @@ def fix_style(po_files, modified=False, no_wrap=False):
     """Fix style of unversionned ``.po`` files, or or all f
     """
     if modified:
-        git_status = check_output(["git", "status", "--porcelain"],
-                                  universal_newlines=True)
-        git_status_lines = [line.split(maxsplit=2) for line in
-                            git_status.split('\n')
-                            if line]
-        po_files.extend(filename for status, filename in git_status_lines
-                        if filename.endswith('.po'))
+        git_status = check_output(
+            ["git", "status", "--porcelain"], universal_newlines=True
+        )
+        git_status_lines = [
+            line.split(maxsplit=2) for line in git_status.split("\n") if line
+        ]
+        po_files.extend(
+            filename
+            for status, filename in git_status_lines
+            if filename.endswith(".po")
+        )
     for po_path in tqdm(po_files, desc="Fixing indentation in po files"):
-        with open(po_path, encoding='UTF-8') as po_file:
+        with open(po_path, encoding="UTF-8") as po_file:
             po_content = po_file.read()
-        args = ['msgcat', '-', '-o', po_path]
+        args = ["msgcat", "-", "-o", po_path]
         if no_wrap:
-            args[1:1] = ['--no-wrap']
+            args[1:1] = ["--no-wrap"]
         print(args)
         run(args, universal_newlines=True, check=True, input=po_content)
 
 
 def main():
     import argparse
+
     parser = argparse.ArgumentParser(
-        description='Ensure po files are using the standard gettext format')
-    parser.add_argument('--modified', '-m', action='store_true',
-                        help='Use git to find modified files.')
-    parser.add_argument('--no-wrap', action='store_true',
-                        help='see `man msgcat`, usefull to sed right after.')
-    parser.add_argument('po_files', nargs='*', help='po files.')
+        description="Ensure po files are using the standard gettext format"
+    )
+    parser.add_argument(
+        "--modified", "-m", action="store_true", help="Use git to find modified files."
+    )
+    parser.add_argument(
+        "--no-wrap",
+        action="store_true",
+        help="see `man msgcat`, usefull to sed right after.",
+    )
+    parser.add_argument("po_files", nargs="*", help="po files.")
     args = parser.parse_args()
     if not args.po_files and not args.modified:
         parser.print_help()
--- setup.py
+++ setup.py
@@ -3,42 +3,35 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    'tqdm'
-]
+requirements = ["tqdm"]
 
 setup(
-    name='poindent',
-    version='0.1.0',
+    name="poindent",
+    version="0.1.0",
     description="Find an properly reindent .po files.",
     long_description=readme,
     author="Julien Palard",
-    author_email='julien@palard.fr',
-    url='https://github.com/JulienPalard/poindent',
-    packages=[
-        'poindent',
-    ],
-    package_dir={'poindent':
-                 'poindent'},
-    entry_points={
-        'console_scripts': ['poindent=poindent.poindent:main']
-    },
+    author_email="julien@palard.fr",
+    url="https://github.com/JulienPalard/poindent",
+    packages=["poindent"],
+    package_dir={"poindent": "poindent"},
+    entry_points={"console_scripts": ["poindent=poindent.poindent:main"]},
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
     zip_safe=False,
-    keywords='poindent',
+    keywords="poindent",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
