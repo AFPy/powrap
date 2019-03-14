e5dc91e32e1e35ef8ff03badbc2492c8dbb115e0
Julien Palard <julien@palard.fr>
2019-03-14 22:29:58 +0100

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as readme_file:
 
 setup(
     name="powrap",
-    version="0.2.3",
+    version="0.3.0",
     description="Find an properly reindent .po files.",
     long_description=readme,
     author="Julien Palard",
