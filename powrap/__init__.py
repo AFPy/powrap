ddd8e7cde3d33d32fd1c6fdd0e9e245db93256c1
Julien Palard <julien@palard.fr>
2019-12-10 10:05:48 +0100

    pylint

--- powrap/__init__.py
+++ powrap/__init__.py
@@ -1,3 +1,8 @@
+"""Script to fix indentation of given ``.po`` files. If ``--modified`` is
+given, it will only fix modified files according to git (usefull if
+your ``.po`` files are versionned).
+"""
+
 __author__ = """Julien Palard"""
 __email__ = "julien@palard.fr"
 __version__ = "0.3.1"
--- powrap/powrap.py
+++ powrap/powrap.py
@@ -3,6 +3,7 @@
 """Fix style of uncommited po files, or all if --all is given.
 """
 
+import argparse
 import sys
 from typing import Iterable
 from pathlib import Path
@@ -51,7 +52,8 @@ def fix_style(po_files, no_wrap=False, quiet=False):
 
 
 def parse_args():
-    import argparse
+    """Parse powrap command line arguments.
+    """
 
     def path(path_str):
         path_obj = Path(path_str)
@@ -99,11 +101,13 @@ def parse_args():
     args = parser.parse_args()
     if not args.po_files and not args.modified:
         parser.print_help()
-        exit(1)
+        sys.exit(1)
     return args
 
 
 def main():
+    """Powrap main entrypoint (parsing command line and all).
+    """
     args = parse_args()
     if args.modified:
         git_status = check_output(["git", "status", "--porcelain"], encoding="utf-8")
